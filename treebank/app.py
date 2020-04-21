e12ddef69ed2044ea17d18849568471cb9555641
Luke Clark M. Andrada <luke.andrada@lenddoefl.com>
2020-04-21 23:05:06 +0800

    Add audio and tree

--- treebank/app.py
+++ treebank/app.py
@@ -1,3 +1,5 @@
+import os
+
 from copy import deepcopy
 from flask import Flask, render_template
 
@@ -5,6 +7,12 @@ from database import db
 
 
 app = Flask(__name__)
+
+audio_dir = 'static/audio'
+os.makedirs(audio_dir, exist_ok=True)
+
+tree_dir = 'static/tree'
+os.makedirs(tree_dir, exist_ok=True)
 
 
 @app.route('/')
@@ -15,6 +23,13 @@ def main():
     for text in raw_texts:
         formatted = deepcopy(text)
 
+        identifier = text['_id']
+        audio_file = f'{tree_dir}/{identifier}.wav'
+        if identifier not in os.listdir(tree_dir):
+            with open(audio_file, 'wb') as audio_bin:
+                audio_bin.write(text['audio_bin'])
+        formatted['audio'] = audio_file
+
         formatted['created'] = text['created'].strftime('%B %d, %Y %X')
         formatted['pos_tagged'] = ', '.join(
             f'{word} - {tag}' for word, tag in text['pos_tagged'])
@@ -22,6 +37,13 @@ def main():
             f'{lhs} -> {" ".join(r for r in rhs)} [{prob}]'
             for lhs, rhs, prob in text['productions'])
         formatted['pcfg'] = f'{text["pcfg"]:.2e}'
+
+        tree_file = f'{tree_dir}/{identifier}.png'
+        if identifier not in os.listdir(tree_dir):
+            with open(tree_file, 'wb') as tree_bin:
+                tree_bin.write(text['tree_bin'])
+        formatted['tree'] = tree_file
+
         texts.append(formatted)
 
     return render_template('data.html', texts=texts)
